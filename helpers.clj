a5f0138db5c6c6bad21f3f1df309f096f67f7648
Jesse Wolfe <jes5199@gmail.com>
2012-04-10 14:47:04 -0700

    some 32bit math

--- helpers.clj
+++ helpers.clj
@@ -6,3 +6,38 @@
   (goto POP)
 )
 
+(defn div32by16 [high low divisor]
+  (concat
+    (DIV low, divisor)
+    (DIV high, divisor)
+    (ADD low, O)
+  )
+)
+
+(defn rightshift32 [high low amount]
+  (concat
+    (SHR low, amount)
+    (SHR high, amount)
+    (ADD low, O)
+  )
+)
+
+(defn div32by32 [high low divisorhigh divisorlow]
+  ; This may be complicated enough to become a function instead of a macro.
+  (let [
+      exit_label  (keyword (gensym "div32by32_exit"))
+      start_label (keyword (gensym "div32by32_start"))
+    ]
+    (concat
+      (label start_label)
+
+      (IFE divisorhigh 0) (goto exit_label)
+      (rightshift32 divisorhigh divisorlow 1)
+      (rightshift32 high low 1)
+      (goto start_label)
+
+      (label exit_label)
+      (div32by16 high low divisorlow)
+    )
+  )
+)
